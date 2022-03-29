0fb5385e4639242ebe305bfb80ca5db32001234b
Kyppy <simanikyppy@gmail.com>
2022-03-30 01:08:53 +0200

    implement print numbers

--- main.cpp
+++ main.cpp
@@ -3,5 +3,39 @@
 using namespace std;
 
 int main () {
+    vector <int> user_integers (0);
+    char menu_selection;
     
+    do
+    {
+       cout << "-----------------------------------------" << endl;
+       cout << "Welcome to menu." << endl;
+       cout << "P - Print numbers" << endl;
+       cout << "A - Add a number" << endl;
+       cout << "M - Display the mean of numbers" << endl;
+       cout << "S - Display the smallest number" << endl;
+       cout << "L - Display the largest number" << endl;
+
+       cout << "Please enter your choice" << endl;
+       cin >> menu_selection;
+
+       switch (menu_selection) {
+           case 'P':
+           case 'p': {
+               if (user_integers.size() < 1) {
+                   cout << "[] - Empty. Please add integers by selecting the 'A' menu option." << endl;
+               } else {
+                   cout << "[ ";
+                   for (auto integer: user_integers){
+                       cout << " " << integer;
+                   }
+                   cout << " ]";
+               }
+               break;
+            default: cout << "Unknown selection please try again" << endl;
+           }
+       }
+    } while (true);
+    
+    return 0;
 }
